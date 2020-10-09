70903a25cedb63d418d200da1033056957947fb5
kevslinger <tug74186@temple.edu>
2020-10-09 09:59:57 -0400

    Implemented transition and reward function, and can save to numpy data file

--- problem6transitionfunction.py
+++ problem6transitionfunction.py
@@ -3,6 +3,7 @@
 
 from scipy.stats import poisson
 import numpy as np
+import pickle
 
 
 
@@ -18,59 +19,45 @@ def create_transition_function():
     poisson_rent_2 = poisson(lambda_rent_2)
     poisson_return_2 = poisson(lambda_return_2)
 
-    for x in range(transition_table):
-        for y in range(transition_table[x]):
-            for z in range(transition_table[x][y]):
-                for w in range(transition_table[x][y][z]):
-                    for a in range(transition_table[x][y][z][w]):
+    for x in range(len(transition_table)):
+        for y in range(len(transition_table[x])):
+            for z in range(len(transition_table[x][y])):
+                for w in range(len(transition_table[x][y][z])):
+                    for v in range(len(transition_table[x][y][z][w])):
+                        # z ranges from 0 to 10, but we want that to be -5 to 5
+                        a = z - 5
                         prob_lot1 = 0
-                        # At this point, we have s = (x, y), a = z, and s2 = (w, a)
+                        # At this point, we have s = (x, y), a = z - 5, and s2 = (w, v)
                         # if a is 5, that means we moved 5 cars from location 2 to location 1
                         # diff is # cars in s2 minus the # of cars in previous state plus cars moved.
                         # If diff is positive, then we got more cars returned than rented out.
                         # if diff is negative, then we rented out more cars than we got returned.
-                        lot1_diff = w - (x + z)
-                        for i in range(x+z+1):
+                        lot1_diff = w - (x + a)
+                        for i in range(x + a + 1):
                             prob_lot1 += poisson_rent_1.pmf(i) * poisson_return_1.pmf(i+lot1_diff)
                         prob_lot1 += 1 - (poisson_rent_1.cdf(x) * poisson_return_1.cdf(x + lot1_diff))
                         # do the same thing for lot 2:
                         prob_lot2 = 0
-                        lot2_diff = a - (y - z)
+                        lot2_diff = v - (y - a)
                         # if lot2_diff is positive, then we gained lot2_diff returns more than rentals
                         # if lot2_diff is negative, then we rented out lot2_diff more than we got returned.
                         # if lot2_diff is 0, then we got the same number rented and returned.
-                        for i in range(y - z + 1):
+                        for i in range(y - a + 1):
                             prob_lot2 += poisson_rent_2.pmf(i) * poisson_return_2.pmf(i + lot2_diff)
                         prob_lot2 += 1 - (poisson_rent_2.cdf(y) * poisson_return_2.cdf(y + lot2_diff))
 
-                        transition_table[x, y, z, w, a] = prob_lot1 * prob_lot2
+                        transition_table[x, y, z, w, v] = prob_lot1 * prob_lot2
+            print("x={}, y={},z={}".format(x, y, a))
+    np.save('transition_function', transition_table)
+
+
+# x = range(21), y = range(21), z = range(11), w = range(21), v = range(21)
 
 
 
 
-
-
-# Function to save the transition function of Jack's car rental environment
-def open_to_close():
-    #
-    #transition_table = np.zeros((21, 21), dtype=np.float32)
-
-    # Give the probability that we end the day with x cars, given we started with y cars.
-    location1_prob_table = np.zeros((21, 26), dtype=np.float32)
-    rental_lambda = 3
-    return_lambda = 3
-    rental_rv = poisson(3)
-    return_rv = poisson(3)
-    for x in range(len(location1_prob_table)):
-        for y in range(len(location1_prob_table[x])):
-            # The probability of having x cars available at the end of the day, given we started with y
-            # This is equal to the sum of probabilities that we rent out some number of cars and get others returned
-            # suppose we start with 15 and end with 10. The prob of that is
-            # the sum of probabilities that: we rent out 5 and get 0 returned.
-            #                               we rent out 6 and get 1 returned.
-            #                                  .... up to renting out 15 and getting 10 returned.
-            location1_prob_table[x, y] = sum([lambda x2, y2: rental_rv.pmf(x2) * return_rv.pmf(y2), range(y - x, y), range(x)])
-
+if __name__ == '__main__':
+    create_transition_function()
 
 # p(s' | s, a) = Pr(S_t = s' | S_t-1 = s, A_t-1 = a} = \sum_{r\in R} p(s', r | s, a)
 # r(s, a) = E[R_t | S_t-1 = s, A_t-1 = a] = \sum_{r \in R} r \sum_{s' \in S} p(s', r | s, a)
